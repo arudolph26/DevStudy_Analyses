8f6c16ac01a41bcf240fe24f10e539dae46e3bb4
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-09 09:49:01 -0700

    working on new options for fit script

--- code/Fit_RL.py
+++ code/Fit_RL.py
@@ -19,7 +19,8 @@ def calculate_prediction_error(x0,data):
     alphapos=x0[0]
     alphaneg=x0[1]
     beta=x0[2]
-    exponent=x0[3]
+    exppos=x0[3]
+    expneg=x0[4]
     choiceprob = np.zeros((len(TrialNum)))
     
     for i in range(len(TrialNum)):
@@ -40,11 +41,11 @@ def calculate_prediction_error(x0,data):
             
             #If the outcome is better than expected use alphapos
             if Outcome[i] > EV[int(TrialNum[i]-1)]:
-                Prediction_Error = alphapos*(Outcome[i] - EV[int(TrialNum[i]-1)])**exponent
+                Prediction_Error = alphapos*(Outcome[i] - EV[int(TrialNum[i]-1)])**exppos
             
             #If the outcome is worst than expected use alphaneg
             if Outcome[i] < EV[int(TrialNum[i]-1)]:
-                Prediction_Error = -1*alphaneg*(EV[int(TrialNum[i]-1)]-Outcome[i])**exponent #have to do it this way because you can't put a negative number to an exponent between 0 and 1
+                Prediction_Error = -1*alphaneg*(EV[int(TrialNum[i]-1)]-Outcome[i])**expneg #have to do it this way because you can't put a negative number to an exponent between 0 and 1
                    
             if Outcome[i] == EV[int(TrialNum[i]-1)]:
                 Prediction_Error = 0
@@ -59,30 +60,38 @@ def calculate_prediction_error(x0,data):
     return(neglogprob)
 
 
-def select_optimal_parameters(subject, n_fits=50, pars):
+def select_optimal_parameters(subject, n_fits=50, pars = {'alpha_pos':np.nan, 'alpha_neg':np.nan, 'beta':np.nan, 'exp_pos':np.nan, 'exp_neg':np.nan}):
     
     data =  pd.read_csv(data_path+'ProbLearn'+str(subject)+'.csv')
     
     Results = pd.DataFrame({'x0_alpha_pos' : np.nan,
                             'x0_alpha_neg' : np.nan,
                             'x0_beta' : np.nan,
-                            'x0_exponent' : np.nan,
+                            'x0_exp_pos' : np.nan,
+                            'x0_exp_neg' : np.nan,
                             'xopt_alpha_pos' : np.nan,
                             'xopt_alpha_neg' : np.nan,
                             'xopt_beta' : np.nan,
-                            'xopt_exponent' : np.nan,
+                            'xopt_exp_pos' : np.nan,
+                            'xopt_exp_neg' : np.nan,
                             'neglogprob' : np.nan}, index = range(n_fits))
     
-    fixparams = ...
-    fitparams = ...
+    fixparams = []
+    fitparams = []
     
     model_name = 'LearningParamsFix_'+ ... + '_Fit_'+ ...
+    
+    for key in pars.keys():
+        if np.isnan(pars[key]):
+            fitparams.append(key)
+        else:
+            fixparams.append(key)
     
     # chenge x0 depending on pars
     
     for i in range(n_fits):
         #Priors
-        x0=[random.uniform(0,.4),random.uniform(0,1)]
+        #x0=[random.uniform(0,.4),random.uniform(0,1)]
         try:
             print(x0)
             
@@ -90,11 +99,11 @@ def select_optimal_parameters(subject, n_fits=50, pars):
             xopt = scipy.optimize.fmin(calculate_prediction_error,x0,args=(data,),xtol=1e-6,ftol=1e-6)
             
             #Update Results output
-            Results.x0_alpha_neg[i]=x0[0]
-            Results.x0_exponent[i]=x0[1]
-            Results.xopt_alpha_neg[i]=xopt[0]
-            Results.xopt_exponent[i]=xopt[1]
-            Results.neglogprob[i] = calculate_prediction_error(xopt,data)
+            #Results.x0_alpha_neg[i]=x0[0]
+            #Results.x0_exponent[i]=x0[1]
+            #Results.xopt_alpha_neg[i]=xopt[0]
+            #Results.xopt_exponent[i]=xopt[1]
+            #Results.neglogprob[i] = calculate_prediction_error(xopt,data)
             
         except:
             print("fmin error")
@@ -102,12 +111,8 @@ def select_optimal_parameters(subject, n_fits=50, pars):
     #write out sorted data
     Results.sort_values(by=['neglogprob']).to_csv(output_path+ model_name+str(subject)+'.csv')
     
-#TO ADD:
-    #Options for different parameters
-    #Model comparison: which 
-    #Stability of parameters for each subject
-    #Consistent age difference (regardless of parameter stability)
-#Imaging:
-    #BIDS validator
-    #MRI-QC
-    #fmri-prep
+# Based on http://zenkavi.github.io/DevStudy/output/reports/ExploratoryDVs.nb.html
+# the age difference in this task lies in learning from high variance negative feedback
+# Which parameter would this be captured in?
+# Well this machine has two properties that are different than the others: the magnitude of the loss and the frequency of loss (sensitivity to EV vs variance?). So either alpha_neg if the RPE from the machine is not weighed enough and/or the exponent on the loss if the loss doesn't feel as severe as it should 
+# Can you fit the model per condition? Probably not since there would only be 45 trials and there should be a more elegant way of capturing this in a single model
