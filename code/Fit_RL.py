43e37be37ed60a5a2b98542cfbd0ad2e6ac9f7eb
Zeynep Enkavi <zeynepenkavi@gmail.com>
2018-05-09 18:28:00 -0700

    first try in fixing the fix/fit parameters in fitting function

--- code/Fit_RL.py
+++ code/Fit_RL.py
@@ -34,18 +34,32 @@ def calculate_prediction_error(x0,data, pars):
     Prediction_Error = 0
     choiceprob = np.zeros((len(TrialNum)))
     
-    #FIGURE OUT HOW TO FIX AND NOT FIT THESE - TRY USING PARS. X0 SHOULD ALREADY ONLY HAVE VALUES FOR THE PARAMETERS THAT WILL BE FITTED
-    
+    #x0 only has values for parameters that will be fitted; so we can't use numerical indices to extract from the list and assign as the parameter value to be used
+    #to fix this we add the argument pars to the function and use it to create to helper lists of parameters that will be fixed and those that will be fit
     fixparams = extract_pars(pars)['fixparams']
     fitparams = extract_pars(pars)['fitparams']
     
-    alphaneg=x0[0]
-    alphapos=x0[1]
-    beta=x0[2]
-    expneg=x0[3]
-    exppos=x0[4]
+    #because the length of x0 and the parameters the values in it correspond to will change depending on what is fixed vs. fit we need a named dictionary
+    #we use the list of fitparams for this
+    x0_dict = dict(zip(fitparams,list(x0)))
     
+    #now we create a dictionary that will contain all the appropriate numbers for each parameter that will be used in the RPE and EV calculations below
+    all_pars_dict = {'alpha_neg':np.nan, 'alpha_pos':np.nan, 'beta':np.nan, 'exp_neg':np.nan, 'exp_pos':np.nan}
     
+    #we populate each element in this dictionary of all to-be-used parameters by going through each key; checking if it is in fixparams. If so we extract is from the params argument in the function call, otherwise we extract the sampled value from x0
+    for par in sorted(all_pars_dict.keys()):
+        if par in fixparams:
+            all_pars_dict[par] = pars[par]
+        else:
+            all_pars_dict[par] = x0_dict[par]
+    
+    #finally we assign the to-be used parameters to objects to avoid too much typing in the rest of the loops
+    alphaneg=all_pars_dict['alpha_neg']
+    alphapos=all_pars_dict['alpha_pos']
+    beta=all_pars_dict['beta']
+    expneg=all_pars_dict['exp_neg']
+    exppos=all_pars_dict['exp_pos']
+      
     for i in range(len(TrialNum)):
         #First update the choice probabilities for each trial
         if Response[i] == 0:
